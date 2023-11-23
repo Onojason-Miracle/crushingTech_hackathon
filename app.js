67623b67ebf7f743591c374a2e93d168c53a8730
Onojason Miracle Ojonuyo <onojasonmiracle@gmail.com>
2023-11-23 23:30:51 +0100

    updating the js

--- app.js
+++ app.js
@@ -8,22 +8,29 @@
 
 
 
+
+
+
 function handleCustomiseClick(setNumber) {
-    const textContainer = document.getElementById(`customise-setup-${setNumber}`).querySelector('.customise-setup-text');
-    const textOne = textContainer.querySelector('.customise-setup-text-one');
-    const imgDiv = document.getElementById(`customise-setup-${setNumber}`).querySelector('.customise-img-div');
-    const text = document.querySelector('.customise-setup-text-onee')
+    const setupDiv = document.querySelector(`#customise-setup-${setNumber}`);
+    const svgDiv = setupDiv.querySelector('.customise-setup-svg-div');
+    const textOne = setupDiv.querySelector('.customise-setup-text-one');
+    const subtextDiv = setupDiv.querySelector('.customise-subtext-div');
+    const themeDiv = setupDiv.querySelector('.customise-theme');
+    const imgDiv = setupDiv.querySelector('.customise-img-div');
+    const customise = setupDiv.querySelector('.customise-setup');
 
-    const setup = document.querySelector(".customise-setup");
+   
+   
+    imgDiv.style.display = "block";
+    subtextDiv.style.display = "block";
+    themeDiv.style.display = "block";
+    textOne.style.fontWeight = "600";
+    customise.style.backgroundColor = "black";
+   
+  }
 
-    // Toggle visibility of text container and image div
-    textContainer.style.display = textContainer.style.display === 'none' ? 'flex' : 'none';
-    imgDiv.style.display = imgDiv.style.display === 'none' ? 'block' : 'none';
 
-    text.style.display = "none";
-    textContainer.style.position="absolute";
-    textContainer.style.marginLeft="2%";
-    setup.style.backgroundColor = "#F3F3F3";
-}
+
 
 
