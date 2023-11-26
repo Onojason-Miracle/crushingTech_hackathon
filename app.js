008c53e8256be8b85b168eda9b6f4b306d0ef504
Onojason Miracle Ojonuyo <onojasonmiracle@gmail.com>
2023-11-26 20:19:58 +0100

    more functionalities

--- app.js
+++ app.js
@@ -1,79 +1,127 @@
-// // Example JavaScript to update the progress bar dynamically
-// var progressBar = document.getElementById('progress-bar');
-// var fraction = 1 / 5; // Change this value to represent the desired fraction
+function handleClearPlans() {
+  const plans = document.querySelector(".section-two-main");
+  const margin = document.querySelector(".margin");
 
-// progressBar.style.width = (fraction * 100) + '%';
+  plans.style.display = "none";
+  margin.style.paddingTop = "3%";
+}
 
-// function handleCustomiseClick(setNumber) {
-//   const setupDiv = document.querySelector(`#customise-setup-${setNumber}`);
-//   const svgDiv = setupDiv.querySelector(".customise-setup-svg-div");
-//   const textOne = setupDiv.querySelector(".customise-setup-text-one");
-//   const subtextDiv = setupDiv.querySelector(".customise-subtext-div");
-//   const themeDiv = setupDiv.querySelector(".customise-theme");
-//   const imgDiv = setupDiv.querySelector(".customise-img-div");
-//   const customise = setupDiv.querySelector("#customise-setup-1");
 
-//   imgDiv.style.display = "block";
-//   subtextDiv.style.display = "block";
-
-//   themeDiv.style.display = "flex";
-//   themeDiv.style.justifyContent = "center";
-//   themeDiv.style.alignItems = "center";
-// //   themeDiv.style.height ="28px;"
-//   textOne.style.fontWeight = "600";
-//   textOne.style.marginTop = "1%";
-// }
 
 
 function handleCustomiseClick(setNumber) {
-    // Iterate over all setup elements to hide their elements
-    document.querySelectorAll('.customise-setup').forEach((setup) => {
-      const imgDiv = setup.querySelector('.customise-img-div');
-      const subtextDiv = setup.querySelector('.customise-subtext-div');
-      const themeDiv = setup.querySelector('.customise-theme');
-  
-      // Hide elements of each setup
-      imgDiv.style.display = 'none';
-      subtextDiv.style.display = 'none';
-      themeDiv.style.display = 'none';
-    });
-  
-    // Show the elements of the clicked setup
-    const setupDiv = document.querySelector(`#customise-setup-${setNumber}`);
-    const imgDiv = setupDiv.querySelector('.customise-img-div');
-    const subtextDiv = setupDiv.querySelector('.customise-subtext-div');
-    const themeDiv = setupDiv.querySelector('.customise-theme');
-    const textOne = setupDiv.querySelector(".customise-setup-text-one");
-    const setup = setupDiv.querySelector(".customise-setup");
-  
-    imgDiv.style.display = 'block';
-    subtextDiv.style.display = 'block';
-    themeDiv.style.display = 'flex';
-    themeDiv.style.alignItems = 'center';
-    themeDiv.style.justifyContent = "center";
-    textOne.style.fontWeight = '600';
-    textOne.style.marginTop = '1%';
-    setup.style.backgroundColor = "red";
-  }
-  
+  document.querySelectorAll(".customise-setup").forEach((setup) => {
+    const imgDiv = setup.querySelector(".customise-img-div");
+    const subtextDiv = setup.querySelector(".customise-subtext-div");
+    const themeDiv = setup.querySelector(".customise-theme");
+    const textOne = setup.querySelector(".customise-setup-text-one");
 
+    // Hiding all the setups before dey are clicked
+    imgDiv.style.display = "none";
+    subtextDiv.style.display = "none";
+    themeDiv.style.display = "none";
+    textOne.style.fontWeight = "500";
+    textOne.style.marginTop = "3.2%";
+  });
 
+  const setupDiv = document.querySelector(`#customise-setup-${setNumber}`);
+  const imgDiv = setupDiv.querySelector(".customise-img-div");
+  const subtextDiv = setupDiv.querySelector(".customise-subtext-div");
+  const themeDiv = setupDiv.querySelector(".customise-theme");
+  const textOne = setupDiv.querySelector(".customise-setup-text-one");
+  const setupWrapper = setupDiv.querySelector(".customise-setup");
 
-
-
-function handleClearPlans(){
-    const plans = document.querySelector(".section-two-main");
-    const margin = document.querySelector(".margin");
-
-    plans.style.display = "none";
-    margin.style.paddingTop = "3%";
+  // Showing all the setups after its clicked
+  imgDiv.style.display = "block";
+  subtextDiv.style.display = "block";
+  themeDiv.style.display = "flex";
+  themeDiv.style.alignItems = "center";
+  themeDiv.style.justifyContent = "center";
+  textOne.style.fontWeight = "600";
+  textOne.style.marginTop = "1%";
+  setupWrapper.style.backgroundColor = "red !important";
 }
 
-// 1. the setup guide icon toggle
-// 2. the check of the boxes
-// 3. the progress bar to follow the checked boxes
+
+
+const progressBar = document.getElementById("progressBar");
+const statusText = document.getElementById("statusText");
+
+let checkboxCount = 0;
+updateStatus();
+
+
+function updateStatus() {
+  const completionPercentage = (checkboxCount / 5) * 100;
+  progressBar.style.width = `${completionPercentage}%`;
+  statusText.textContent = `${checkboxCount} / 5 completed`;
+}
+
+
+
+function handleSvgClick(svgElement) {
+  svgElement.classList.toggle("checked");
+  checkboxCount = document.querySelectorAll(".customise-svg.checked").length;
+  updateStatus();
+
+  const svgCircle = svgElement.querySelector("circle");
+  if (svgElement.classList.contains("checked")) {
+    svgCircle.style.display = "none";
+    svgElement.innerHTML =
+      '<image href="https://crushingit.tech/hackathon-assets/icon-checkmark-circle.svg" width="32" height="32" />';
+  } else {
+    svgElement.innerHTML =
+      '<circle cx="16" cy="16" r="12" stroke="grey" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="4 6" />';
+  }
+}
+
+
+
+function toggleSetups() {
+  const setupSvg = document.querySelector(".setup-svg");
+  const setupSvgTwo = document.querySelector(".setup-svg-two");
+  const setups = document.getElementById("setups");
+  const setUpDiv = document.querySelector(".setup-svg-div");
+
+  const setupsDisplayStyle = window
+    .getComputedStyle(setups)
+    .getPropertyValue("display");
+
+  if (setups.style.display === "none") {
+    setups.style.display = "block";
+    setupSvg.style.display = "none";
+    setupSvgTwo.style.display = "block";
+  } else {
+    setups.style.display = "none";
+    setupSvg.style.display = "block";
+    setupSvgTwo.style.display = "none";
+  }
+}
+
+function hover() {
+  const setUpDiv = document.querySelector(".setup-svg-div");
+
+  setUpDiv.style.backgroundColor = "#F3F3F3";
+  setUpDiv.style.borderRadius = "5px";
+}
+
+
+document.querySelector(".setup-svg-div").addEventListener("mouseover", hover);
+
+function unhover() {
+  const setUpDiv = document.querySelector(".setup-svg-div");
+
+  setUpDiv.style.backgroundColor = "";
+  setUpDiv.style.borderRadius = "";
+}
+
+document.querySelector(".setup-svg-div").addEventListener("mouseout", unhover);
+
+// 1. the setup guide icon toggle - done
+// 2. the check of the boxes - done
+// 3. the progress bar to follow the checked boxes - done
 // 4. animation on the boxes
 // 5. background color on display of the contents of the boxes
-// 6. when one box is displaying a content, the others should Not
+// 6. when one box is displaying a content, the others should Not - done
 // 7. responsiveness
 // 8. accessibility to keyboard users
